c3fe381d2966263c92cb961925246a2f63bd4f02
rohith7livingston <144409484+rohith7livingston@users.noreply.github.com>
2025-02-28 18:44:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
     <main id="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/login" element={< Login/>} />
-          <Route path="/home" element={< Home />} />
+          <Route path="/" element={< Home />} />
           <Route path="/create-note" element={< CreateNote/>} />
           <Route path="/voice" element={< Voice/>} />
           <Route path="/edit-note" element={<EditNote />} />
